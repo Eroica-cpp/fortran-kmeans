4099692ee4cdf471b96002b84d1d3c0efbc742d2
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-15 09:58:15 +0800

    update get_id_codeList method

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Fortran K-means
 A Fortran implement of k-means algorithm, which is good at dealing with high
 dimension but sparse matrices, espcially designed for NLP problems.
 
-
+Prerequisite
+-----------------------
+A table contains (page id, key word list) pairs.
---
+++ preprocess.py
@@ -0,0 +1,25 @@
+"""
+preprocess and clean data
+"""
+import cPickle
+import sys
+
+def get_id_codeList():
+	"""
+	replace every key word in key_word_dict as a code
+	"""
+	word2code = cPickle.load(open("./data/word_to_code.pickle"))
+	key_word_dict = cPickle.load(open("./data/key_word_dict.pickle"))
+
+	f = open("./data/id_codeList.txt", "w")
+	for (iden, key_word_list) in key_word_dict.items():
+		code_list = []
+		for key in key_word_list:
+			code_list.append(str(word2code[key]))
+		f.write(str(iden) + "\t" + "\t".join(code_list) + "\n")
+		print "iden:", iden, "done!"
+
+	f.close()
+
+if __name__ == "__main__":
+	get_id_codeList()
