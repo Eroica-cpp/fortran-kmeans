83d95f6d9165c849ae6e12242705124926e34b11
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-15 11:25:03 +0800

    update Pseudocode

---
+++ kmeans.f90
@@ -0,0 +1,37 @@
+! k-means algorithm
+! Author: Tao Li
+! email: eroicacmcs@163.com
+!
+! Pseudocode:
+!***********************************!
+! var centroid_list					!
+! var points						!
+! 									!
+! centroid_list.init()				!
+! points.init()						!
+!									!
+! while(terminate condition){		!
+! 									!
+! 	for point in points {			!
+! 		point.update_lable()		!
+! 	}								!
+! 	centroid_list.update()			!
+! }									!
+!***********************************
+
+program kmeans
+
+	implicit none
+
+	centroid_list
+	points
+
+	centroid_list.init()
+
+	for point in points
+		point.getlable
+
+		if (..):
+			terminate
+
+end program kmeans 
